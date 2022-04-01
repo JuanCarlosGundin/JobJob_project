599238386b3ff0ea04edeb8407bcfbde63549559
LauraFernandez18 <10000068.joan23@fje.edu>
2022-04-01 14:39:09 +0200

    comienzo css login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,39 +1,31 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <title>Login proyectofinal</title>
-  <meta charset="UTF-8">
-  {{-- <link rel="stylesheet" href="{!! asset('css/styles.css') !!}"> --}}
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Login</title>
+    <link rel="stylesheet" href="{!! asset('css/style.scss') !!}">
 </head>
-<body class="login">
-  <div class="row flex-cv">
-    <div class="cuadro_login">
-        <form action="{{url('login')}}" method="POST">
-            @csrf
-            <br>
-            <h1>INICIO DE SESIÓN</h1>
-            <br>
-            <div class="form-group">
-                <p>Correo:</p>
-                <div>
-                    <input class="inputlogin" type="text" name="mail" placeholder="Introduce tu correo">
-                </div>
-            </div>
-            <br>
-            <div class="form-group">
-                <p>Contraseña:</p>
-                <div>
-                    <input class="inputlogin" type="password" name="contra" placeholder="Introduce tu contraseña">
-                </div>
-            </div>
-            
-            <br><br>
-            <div class="form-group">
-                <button class= "botonlogin" type="submit" value="register">Iniciar Sesión</button>
-            </div>
-        </form>
+<body>
+    <div class="logo">
+        <img src="storage/uploads/icon.jpg">
     </div>
-  </div>
+    <div class="modal-login">
+        <h1>Iniciar sesión       Registrar</h1>
+            <div class="modal-content">
+            <form action="{{url('login')}}" method="POST">
+                @csrf
+                <h2>JobJob es innovador</h2>
+                    <input class="inputlogin" type="text" name="mail" placeholder="Introduce tu correo"><br>
+                    <input class="inputlogin" type="password" name="contra" placeholder="Introduce tu contraseña"><br>
+                    <button class= "botonlogin" type="submit" value="register">Iniciar Sesión</button>
+                    <p>¿contraseña olvidada?</p>
+                    <div class="linea"></div>
+                    <p>Cónectate con Google</p>
+            </form>
+            </div>
+    </div>
 </body>
 </html>
